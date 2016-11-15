d66d360254d05ae068731ea301201b70024654c0
Kmiyagi <rate98326@gmail.com>
2016-11-15 23:22:14 +0900

    リファクタ

--- AccelTest/AccelTest.ino
+++ AccelTest/AccelTest.ino
@@ -6,9 +6,9 @@
 #include <math.h>
 #include <MsTimer2.h>
 
-#define I2C_ADDRES_ACCEL 0x1D		//加速度センサー(ADXL345)
-#define I2C_ADDRES_COMPASS 0x1E		//地磁器センサー(HMC5883L)
-#define I2C_ADDRES_AIRSTATE 0x77	//温度、湿度、大気圧センサー(BME280)
+#define I2C_ADDRES_ACCEL 0x1D       //加速度センサー(ADXL345)
+#define I2C_ADDRES_COMPASS 0x1E     //地磁器センサー(HMC5883L)
+#define I2C_ADDRES_AIRSTATE 0x77    //温度、湿度、大気圧センサー(BME280)
 
 #define COMPASS_DATA_X 0x03
 #define COMPASS_DATA_Y 0x07
@@ -19,16 +19,18 @@
 enum{X, Y, Z};
 enum{TEMP, PRESS, HUM};
 
-float speed_x=0.0, speed_y=0.0;
-long cm_x=0, cm_y=0;
-int16_t raw_x, raw_y;
-int16_t low_x, low_y, low_z;
+volatile int16_t speed_x, speed_y;
+volatile long cm_x=0, cm_y=0;
+volatile int16_t raw_x, raw_y;
+volatile int16_t low_x=0, low_y=0, low_z;
+volatile int16_t hx, hy;
+volatile float fx, fy;
 
 void setup()
 {
     Wire.begin();
-    writeI2C(I2C_ADDRES_ACCEL, 0x31, 0x00);
-    writeI2C(I2C_ADDRES_ACCEL, 0x2D, 0x08);
+    writeI2C(I2C_ADDRES_ACCEL, 0x31, 0x00); //range +-2g
+    writeI2C(I2C_ADDRES_ACCEL, 0x2D, 0x08); //sleep off
     
     Serial.begin(9600);
     Serial.println();
@@ -61,11 +63,26 @@ void Accel_integral2() {
     raw_y = ByteToInt(data[2], data[3]);
 
     //ローパスフィルタ
-    low_x = 9 * raw_x + low_x;
-    low_y = 9 * raw_y + low_y;
+    low_x += 9 * raw_x;
+    low_y += 9 * raw_y;
+	
+	low_x /= 10;
+	low_y /= 10;
 
+	fx = low_x / 256.0;
+	fy = low_y / 256.0;
+/*	
+	fx = raw_x / 256.0;
+	fy = raw_y / 256.0;
+    hx = raw_x - low_x;
+    hy = raw_y - low_y;
+    
+    speed_x += hx*100/256;
+    speed_y += hy*100/256;
+*/  
+    
 /*    cm_x += raw;
-	cm_y += low_y / 256.0;*/
+    cm_y += low_y / 256.0;*/
 }
 
 void Accel_integral() {
@@ -104,9 +121,9 @@ void Accel_integral_print() {
 }
 
 void Accel_integral_print2() {
-    Serial.print(low_x);
+    Serial.print(fx);
     Serial.print(",");
-    Serial.println(low_y);
+    Serial.println(fy);
 }
 
 #if 0
@@ -129,7 +146,7 @@ void loop()
 }
 #endif
 /*********************************************************************/
-/*	   ADXL345														 */
+/*     ADXL345                                                       */
 /*********************************************************************/
 void getAccel(float accel_data[])
 {
@@ -162,7 +179,7 @@ void getAccel(float accel_data[])
 }
 
 /*********************************************************************/
-/*	   I2Cread/write関数											 */
+/*     I2Cread/write関数                                             */
 /*********************************************************************/
 void writeI2C(byte I2C_addres, byte register_addres, byte value)
 {
